78c0326a0897e5cb0f3076a43e1a6c84efe499d2
ferron <ferronrsmith@gmail.com>
2014-11-30 23:22:22 -0400

    added documentation for spat

--- docs/Spat.md
+++ docs/Spat.md
@@ -2,42 +2,73 @@
 =====
 
 
+ Spat is a facade for the JSQLParser that offers an easy API that is used by the CLI, GUI front-ends.
+
+
+
 
 ###Methods
 - [generateXpath(String sql)](#-1659832918)  returns String
-- [getElements(String xmlfile, String sql)](#-1039089193)  returns List
 - [parse(String xmlfile, String sql, ResultType type, String outputFile)](#-1099670828) 
 - [parseXML(String xmlfile, String sql, ResultType type, String outputFile)](#-1075670907)  returns List
 
 
 #### <span style="font-size:12px;color:#AAAAAA">String</span> <a style="font-size:16px;" name="-1659832918">generateXpath</a><span style="font-size:16px;">(String sql)</span>
 - <b>sql</b>: 
-        the sql
+        the sql statement
 
-- <b>returns</b>: the string
+- <b>returns</b>: unvalidated xpath expression
 
-#### <span style="font-size:12px;color:#AAAAAA">List&lt;org.dom4j.Element&gt;</span> <a style="font-size:16px;" name="-1039089193">getElements</a><span style="font-size:16px;">(String xmlfile, String sql)</span>
+Generates an XPATH expression from a given SQL statement.
+If a valid SQL query is passed to Spat it will parse that expression
+creating visitors which are then traversed. An Expression Tree is generated
+from the SQL segments which are organized and converted to an XPATH expression.
+If an invalid SQL statement is passed an exception will be thrown. This method
+is mainly a helper method that is used by different interfaces & methods
+> Please note that this expression has not been validated by **QueryXML** class
+
+
 
 #### <a style="font-size:16px;" name="-1099670828">parse</a><span style="font-size:16px;">(String xmlfile, String sql, ResultType type, String outputFile)</span>
 - <b>xmlfile</b>: 
-        the xmlfile
+        the xml file to be queried
 - <b>sql</b>: 
-        the sql
+        the sql statement
 - <b>type</b>: 
-        the type
+        the ResultType to be returned. Please see <a href="https://github
+        .com/ferronrsmith/jsqltoxml/blob/master/docs/ResultType.md"></a> for a listing of all supported types
 - <b>outputFile</b>: 
-        the output file
+        the output file results are to be written to.
+
+
+The following function takes an sql statement which is then parsed.
+Unlike the *generateXpath* method, validation is performed on the XPATH
+expression after generation. If the XPATH expression is valid, it will then
+be passed to the internal XPATH Querying library which will query the supplied
+XML file. The output is written to the terminal (CLI) and written to
+an output file if supplied.
+
 
 
 #### <span style="font-size:12px;color:#AAAAAA">List&lt;String&gt;</span> <a style="font-size:16px;" name="-1075670907">parseXML</a><span style="font-size:16px;">(String xmlfile, String sql, ResultType type, String outputFile)</span>
 - <b>xmlfile</b>: 
-        the xmlfile
+        the xml file to be queried
 - <b>sql</b>: 
-        the sql
+        the sql statement
 - <b>type</b>: 
-        the type
+        the ResultType to be returned. Please see <a href="https://github
+        .com/ferronrsmith/jsqltoxml/blob/master/docs/ResultType.md"></a> for a listing of all supported types
 - <b>outputFile</b>: 
-        the output file
+        the output file results are to be written to.
 
-- <b>returns</b>: the list
+- <b>returns</b>: A list of xml nodes that match the xpath expression
+
+The following function takes an sql statement which is then parsed.
+Unlike the *generateXpath* method, validation is performed on the XPATH
+expression after generation. If the XPATH expression is valid, it will then
+be passed to the internal XPATH Querying library which will query the supplied
+XML file. The output is written as a list of string and written to
+an output file if supplied.
+
+
 
--- src/main/java/com/uwi/engine/Spat.java
+++ src/main/java/com/uwi/engine/Spat.java
@@ -15,17 +15,26 @@ import org.dom4j.Element;
 import java.util.List;
 
 /**
- * The Class JSql.
+ * <code>
+ *  Spat is a facade for the JSQLParser that offers an easy API that is used by the CLI, GUI front-ends.
+ * </code>
  */
 public final class Spat {
 
     /**
-     * Xpath string.
-     *
+     * <code>
+     * Generates an XPATH expression from a given SQL statement.
+     * If a valid SQL query is passed to Spat it will parse that expression
+     * creating visitors which are then traversed. An Expression Tree is generated
+     * from the SQL segments which are organized and converted to an XPATH expression.
+     * If an invalid SQL statement is passed an exception will be thrown. This method
+     * is mainly a helper method that is used by different interfaces & methods
+     * > Please note that this expression has not been validated by **QueryXML** class
+     *</code>
      * @param sql
-     *         the sql
+     *         the sql statement
      *
-     * @return the string
+     * @return unvalidated xpath expression
      *
      * @throws JSQLParserException
      *         the JSQL parser exception
@@ -35,16 +44,24 @@ public final class Spat {
     }
 
     /**
-     * Parses the sql query and return a specific type.
+     * <code>
+     * The following function takes an sql statement which is then parsed.
+     * Unlike the *generateXpath* method, validation is performed on the XPATH
+     * expression after generation. If the XPATH expression is valid, it will then
+     * be passed to the internal XPATH Querying library which will query the supplied
+     * XML file. The output is written to the terminal (CLI) and written to
+     * an output file if supplied.
+     * </code>
      *
      * @param xmlfile
-     *         the xmlfile
+     *         the xml file to be queried
      * @param sql
-     *         the sql
+     *         the sql statement
      * @param type
-     *         the type
+     *         the ResultType to be returned. Please see <a href="https://github
+     *         .com/ferronrsmith/jsqltoxml/blob/master/docs/ResultType.md"></a> for a listing of all supported types
      * @param outputFile
-     *         the output file
+     *         the output file results are to be written to.
      *
      * @throws JSQLParserException
      *         the JSQL parser exception
@@ -62,18 +79,26 @@ public final class Spat {
     }
 
     /**
-     * Parses the xml.
+     * <code>
+     * The following function takes an sql statement which is then parsed.
+     * Unlike the *generateXpath* method, validation is performed on the XPATH
+     * expression after generation. If the XPATH expression is valid, it will then
+     * be passed to the internal XPATH Querying library which will query the supplied
+     * XML file. The output is written as a list of string and written to
+     * an output file if supplied.
+     * </code>
      *
      * @param xmlfile
-     *         the xmlfile
+     *         the xml file to be queried
      * @param sql
-     *         the sql
+     *         the sql statement
      * @param type
-     *         the type
+     *         the ResultType to be returned. Please see <a href="https://github
+     *         .com/ferronrsmith/jsqltoxml/blob/master/docs/ResultType.md"></a> for a listing of all supported types
      * @param outputFile
-     *         the output file
+     *         the output file results are to be written to.
      *
-     * @return the list
+     * @return A list of xml nodes that match the xpath expression
      *
      * @throws JSQLParserException
      *         the JSQL parser exception
@@ -92,6 +117,12 @@ public final class Spat {
         Misc.saveToFile(outputFile, result);
         return result;
     }
+
+    /**
+     * Used for testing purposes only. this method return element ResultType
+     * @internal
+     * @throws JSQLParserException
+     */
     public static List<Element> getElements(String xmlfile, String sql) throws JSQLParserException {
         DefaultSelectVisitor visitor = visit(sql);
         return new QueryXML().query(
